7b92ecc6814d8d7a42ca7efffa280a3e7552b883
Avinash-1-10 <avinashyede110@gmail.com>
2024-04-08 12:00:43 +0530

    map errors removed

--- src/components/OngoingServices.tsx
+++ src/components/OngoingServices.tsx
@@ -3,7 +3,7 @@ import ServiceCard from "./ServiceCard";
 const OngoingServices = () => {
   return (
     <div className='flex flex-col gap-8'>
-      {[1, 2, 3, 4, 4, 5, 66, 6, 66, 2, 3, 3, 3].map((t, i) => (
+      {[1, 2, 3, 4, 4, 5, 66, 6, 66, 2, 3, 3, 3].map(() => (
         <ServiceCard />
       ))}
     </div>
